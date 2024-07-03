3d220de634e1a7ca831d84b60a0e31caa1694cb1
Matej Kvassay <matejkvassay5@gmail.com>
2024-07-03 12:49:02 +0200

    docs

--- matejchain/msg.py
+++ matejchain/msg.py
@@ -23,19 +23,36 @@ class Msg:
         self.role = role
         self.openai_fmt = {"role": self.role, "content": self.content}
 
-    def to_openai(self):
+    def to_openai(self) -> dict:
+        """
+        Get dict compatible with OpenAI completions api message format.
+
+        :return: dict fmt {"role": <ROLE>, "content": <CONTENT>}
+        """
         return self.openai_fmt
 
-    def is_sys_msg(self):
+    def is_sys_msg(self) -> bool:
+        """
+        True if this message is System message.
+        """
         return self.role == ROLE_SYSTEM
 
     def is_usr_msg(self):
+        """
+        True if this message is User message.
+        """
         return self.role == ROLE_USER
 
     def is_ass_msg(self):
+        """
+        True if this message is Assistant message.
+        """
         return self.role == ROLE_ASSISTANT
 
     def is_tool_msg(self):
+        """
+        True if this message is Tool message.
+        """
         return self.role == ROLE_TOOL
 
     def __str__(self):
@@ -47,23 +64,39 @@ class Msg:
 
 class SysMsg(Msg):
     """
-    System message
+    System message. Usually used in beginning of chat to configure persona and basic
+    instructions to condition the chat LLM to alter its behaviour and tone.
     """
 
-    def __init__(self, content):
+    def __init__(self, content: str):
+        """
+        :param content: Message content.
+        """
         super().__init__(ROLE_SYSTEM, content)
 
 
 class UsrMsg(Msg):
+    """
+    User message. Represents messages from chatbot user.
+    """
+
     def __init__(self, content):
         super().__init__(ROLE_USER, content)
 
 
 class AssMsg(Msg):
+    """
+    Assistant message. Represents messages from the LLM chatbot.
+    """
+
     def __init__(self, content):
         super().__init__(ROLE_ASSISTANT, content)
 
 
 class ToolMsg(Msg):
+    """
+    Represents output from tool execution.
+    """
+
     def __init__(self, content):
         super().__init__(ROLE_TOOL, content)
