95a83307e4515a28aafc2efa29375521fcf20222
Matej Kvassay <matejkvassay5@gmail.com>
2024-07-14 19:59:02 +0200

    chainlit demo

--- examples/chainlit_chat_agent/README.md
+++
@@ -1,4 +0,0 @@
-Example of chainlit chatbot app.
-```commandline
-pip install chainlit
-```
---
+++ examples/simple_chatbot/app.py
@@ -0,0 +1,24 @@
+import chainlit as cl
+from matejchain.chat_hist import ChatHist
+from matejchain.llm import LLM
+from matejchain.chat_agent import ChatAgent
+
+CHAT_HIST_LIMIT = 5
+SYSTEM_PROMPT = "Your name is Mr. Jester Funnybot, always end your answer with a joke!"
+agent = None
+
+
+@cl.on_chat_start
+def on_chat_start():
+    global agent
+    hist = ChatHist(limit=CHAT_HIST_LIMIT, sys_msg=SYSTEM_PROMPT)
+    llm = LLM("gpt-3.5-turbo")
+    agent = ChatAgent(llm=llm, chat_hist=hist)
+
+
+@cl.on_message
+async def main(message: cl.Message):
+    response = agent.chat(message.content).content
+    await cl.Message(
+        content=response,
+    ).send()
